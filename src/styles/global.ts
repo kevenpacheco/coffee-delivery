7fcb9ce2e7074eb82730cfaec90455eb0ecc4ce0
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-11-24 11:47:27 -0300

    refactor: add style button tag default and add default line height of texts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,11 +14,17 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: ${({theme}) => theme.color.base.background};
     color: ${({theme}) => theme.color.base.text};
+    line-height: 130%;
   }
 
   h1, h2, h3, h4, h5, h6 {
     font-family: 'Baloo 2', cursive;
     font-weight: 800;
     line-height: 130%;
+  }
+
+  button {
+    cursor: pointer;
+    border: 0;
   }
 `;
