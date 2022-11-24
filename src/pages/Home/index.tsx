5a364c1f432c9502628f27368e235a5431b27c3a
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-11-24 11:49:19 -0300

    feat: finalize homepage styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,84 @@
+import { ShoppingCart, Timer, Package, Coffee } from "phosphor-react";
+import {
+  CoffeeList,
+  CoffeesContainer,
+  Container,
+  DescriptionItem,
+  DescriptionsContainer,
+  IntroContainer,
+  TitleContainer,
+} from "./styles";
+import { coffees as coffeesMock } from "../../mocks/coffees";
 import { Header } from "../../components/Header";
+import introIMG from "../../assets/intro-image.png";
+import { CoffeeCard } from "../../components/CoffeeCard";
 
 export function Home() {
   return (
+    <Container>
       <Header />
+
+      <IntroContainer>
+        <div>
+          <div>
+            <TitleContainer>
+              <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+              <p>
+                Com o Coffee Delivery você recebe seu café onde estiver, a
+                qualquer hora
+              </p>
+            </TitleContainer>
+
+            <DescriptionsContainer>
+              <DescriptionItem>
+                <div>
+                  <ShoppingCart weight="fill" />
+                </div>
+                <p>Compra simples e segura</p>
+              </DescriptionItem>
+
+              <DescriptionItem>
+                <div>
+                  <Timer weight="fill" />
+                </div>
+                <p>Embalagem mantém o café intacto</p>
+              </DescriptionItem>
+
+              <DescriptionItem>
+                <div>
+                  <Package weight="fill" />
+                </div>
+                <p>Entrega rápida e rastreada</p>
+              </DescriptionItem>
+
+              <DescriptionItem>
+                <div>
+                  <Coffee weight="fill" />
+                </div>
+                <p>O café chega fresquinho até você</p>
+              </DescriptionItem>
+            </DescriptionsContainer>
+          </div>
+
+          <img
+            src={introIMG}
+            alt="Um copo de café branco, com tampa preta e uma faixa preta no meio do copo com a logo da empresa e grãos de café ao redor do copo"
+            width={476}
+            height={360}
+          />
+        </div>
+      </IntroContainer>
+
+      <CoffeesContainer>
+        <div>
+          <h2>Nossos cafés</h2>
+          <CoffeeList>
+            {coffeesMock.map((coffee) => (
+              <CoffeeCard key={coffee.id} {...coffee} />
+            ))}
+          </CoffeeList>
+        </div>
+      </CoffeesContainer>
+    </Container>
   );
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,113 @@
+import styled from "styled-components";
+import bgIntroImg from '../../assets/intro-background.png'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const IntroContainer = styled.section`
+  width: 100%;
+  display: grid;
+  place-items: center;
+  min-height: 34rem;
+  background: url(${bgIntroImg}) no-repeat center;
+  background-size: cover;
+  padding: ${({theme}) => `0 ${theme.layout.paddingX}`};
+  
+  > div {
+    width: ${({theme}) => theme.layout.width};
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    @media (max-width: 1120px) {
+      > img {
+        display: none;
+      }
+    }
+  }
+`;
+
+export const TitleContainer = styled.div`
+  max-width: 36.75rem;
+
+  h1 {
+    color: ${({theme}) => theme.color.base.title};
+    font-size: ${({theme}) => theme.font.size.title.xl};
+  }
+  
+  p {
+    margin-top: 1rem;
+    font-size: ${({theme}) => theme.font.size.text.xl};
+    line-height: 130%;
+    color: ${({theme}) => theme.color.base.subtitle};
+  }
+`;
+
+export const DescriptionsContainer = styled.div`
+  margin-top: 4.125rem;
+  display: grid;
+  grid-template-columns: repeat(2, max-content);
+  gap: 1.25rem 2.5rem;
+`;
+
+export const DescriptionItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: .75rem;
+  
+  div {
+    padding: .5rem;
+    border-radius: 50%;
+    color: ${({theme}) => theme.color.base.background};
+    display: grid;
+    place-items: center;
+  }
+
+  &:nth-child(1) {
+    div {
+      background: ${({theme}) => theme.color.yellow[700]}
+    }
+  }
+
+  &:nth-child(2) {
+    div {
+      background: ${({theme}) => theme.color.base.text}
+    }
+  }
+
+  &:nth-child(3) {
+    div {
+      background: ${({theme}) => theme.color.yellow[500]}
+    }
+  }
+
+  &:nth-child(4) {
+    div {
+      background: ${({theme}) => theme.color.purple[500]}
+    }
+  }
+`;
+
+export const CoffeesContainer = styled.section`
+  width: ${({theme}) => theme.layout.width};
+  padding: 2rem 0;
+
+  & > div {
+    padding: ${({theme}) => `0 ${theme.layout.paddingX}`}
+  }
+
+  h2 {
+    color: ${({theme}) => theme.color.base.subtitle};
+    font-size: ${({theme}) => theme.font.size.title.lg};
+  }
+`;
+
+export const CoffeeList = styled.ul`
+  margin-top: 3.375rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(15.25rem, 1fr));
+  gap: 2.5rem 2rem;
+`;
