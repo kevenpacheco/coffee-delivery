e93cd28dbed872bb96830d94a3bf17851d20d598
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-11-25 00:09:24 -0300

    refactor (CountButton): remove state

--- src/components/Buttons/CountButton/index.tsx
+++ src/components/Buttons/CountButton/index.tsx
@@ -1,31 +1,23 @@
 import { Minus, Plus } from "phosphor-react";
-import { useState } from "react";
 import { Container, SIZES_MAP } from "./styles";
 
 interface CountButtonPropsType {
+  count: number;
   size?: keyof typeof SIZES_MAP;
+  onIncrement: () => void;
+  onDecrement: () => void;
 }
 
-export function CountButton({ size = "md" }: CountButtonPropsType) {
-  const [count, setCount] = useState(1);
-
-  function handleDecrementCount() {
-    setCount(prevState => prevState - 1)
-  }
-
-  function handleIncrementCount() {
-    setCount(prevState => prevState + 1)
-  }
-
+export function CountButton({ count, size = "md", onDecrement, onIncrement }: CountButtonPropsType) {
   return (
     <Container size={size}>
-      <button type="button" onClick={handleDecrementCount} disabled={count === 1}>
+      <button type="button" onClick={onDecrement} disabled={count === 1}>
         <Minus weight="bold" size={14} />
       </button>
 
       <span>{count}</span>
 
-      <button type="button" onClick={handleIncrementCount}>
+      <button type="button" onClick={onIncrement}>
         <Plus weight="bold" size={14} />
       </button>
     </Container>
