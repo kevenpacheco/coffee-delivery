1342e73b664cd5fe17171150ca0abff8d106f3d1
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-11-29 14:27:04 -0300

    feat: crete scroll to top hook

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import { useScrollToTop } from "./hooks/useScrollToTop";
 import { Checkout } from "./pages/Checkout";
 import { Home } from "./pages/Home";
 import { Success } from "./pages/Success";
 
 export function AppRoutes() {
+  useScrollToTop();
+
   return (
     <Routes>
       <Route path="/" element={<Home />}/>
